1efd511304d91275c25ee3956ac2d3c3c1059846
Cody Lerum <cody.lerum@gmail.com>
2014-02-13 21:06:27 -0700

    track sequeneces per timestamp to elimiate duplicates simplify

--- src/main/java/com/outjected/simpleflake/FlakeGenerator.java
+++ src/main/java/com/outjected/simpleflake/FlakeGenerator.java
@@ -1,31 +1,73 @@
 package com.outjected.simpleflake;
 
+import java.nio.ByteBuffer;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FlakeGenerator {
 
   private static final long EPOCH = 1288834974657L;
   private static final long MAX_SIGNED_LONG = 2199023255551L;
-  private static final long MAX_RANDOM_BOUND = 4194305;
 
-  public static SimpleFlake generate() {
+  private static final int TIME_SHIFT = 22;
+  private static final int RANDOM_SHIFT = 42;
 
-    long current = System.currentTimeMillis();
+  private long lastTimestamp = 0;
+  private Set<Long> recentRandoms = new HashSet<>(5000);
 
-    if (current < EPOCH || current > MAX_SIGNED_LONG) {
-      // The current time cannot be less than the EPOCH
-      throw new RuntimeException("Invalid System Clock was " + new Date(current));
+  /**
+   * Generates a Twitter Snowflake compatible id utilizing randomness for the right most 22 bits and
+   * the Twitter standard EPOCH
+   * 
+   * @return long
+   */
+  public long generate() {
+
+    long currentTimestamp = System.currentTimeMillis();
+
+    while (lastTimestamp > currentTimestamp) {
+      // Clock is running backwards so wait until it isn't
+      currentTimestamp = System.currentTimeMillis();
     }
 
-    final long timestamp = current - EPOCH;
+    if (currentTimestamp < EPOCH || currentTimestamp > MAX_SIGNED_LONG) {
+      // The current time cannot be less than the EPOCH
+      throw new RuntimeException("Invalid System Clock was " + new Date(currentTimestamp));
+    }
 
-    long shiftedTimeStamp = timestamp << 22;
+    final long customTimestamp = currentTimestamp - EPOCH;
 
-    long random = ThreadLocalRandom.current().nextLong(MAX_RANDOM_BOUND);
+    final long shiftedTimestamp = customTimestamp << TIME_SHIFT;
 
-    long shiftedResult = shiftedTimeStamp | random;
+    long random = nextRandomPart();
 
-    return new SimpleFlake(shiftedResult);
+    if (lastTimestamp != currentTimestamp) {
+      // timestamp has advanced so reset it and clear the previous cache
+      lastTimestamp = currentTimestamp;
+      recentRandoms.clear();
+    } else {
+      // Same timestamp as previous keep generating randoms till new is found
+      while (recentRandoms.contains(random)) {
+        random = nextRandomPart();
+      }
+    }
+    recentRandoms.add(random);
+    return shiftedTimestamp | random;
+  }
+
+  /**
+   * Generates a Twitter Snowflake compatible id utilizing randomness for the right most 22 bits and
+   * the Twitter standard EPOCH
+   * 
+   * @return byte[]
+   */
+  public byte[] generateBytes() {
+    return ByteBuffer.allocate(8).putLong(generate()).array();
+  }
+
+  private long nextRandomPart() {
+    return ThreadLocalRandom.current().nextLong() >>> RANDOM_SHIFT;
   }
 }
--- src/test/java/com/outjected/simpleflake/GenerationTest.java
+++ src/test/java/com/outjected/simpleflake/GenerationTest.java
@@ -1,6 +1,8 @@
 package com.outjected.simpleflake;
 
 
+import java.util.HashSet;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,8 +10,21 @@ public class GenerationTest {
 
   @Test
   public void test() {
-    SimpleFlake sf = FlakeGenerator.generate();
-    Assert.assertTrue(sf.toByteArray().length == 8);
-    Assert.assertNotEquals(FlakeGenerator.generate().toLong(), FlakeGenerator.generate().toLong());
+    FlakeGenerator gen = new FlakeGenerator();
+    Assert.assertTrue(gen.generateBytes().length == 8);
+    Assert.assertNotEquals(gen.generate(), gen.generate());
+  }
+
+  @Test
+  public void generateAMillion() {
+    final int ITERATIONS = 1_000_000;
+    FlakeGenerator gen = new FlakeGenerator();
+    HashSet<Long> results = new HashSet<>(ITERATIONS, 100);
+    for (int i = 0; i < ITERATIONS; i++) {
+      results.add(gen.generate());
+    }
+
+    // Since a Set can't have duplicates there must be exactly 1,000,000
+    Assert.assertEquals(ITERATIONS, results.size());
   }
 }
